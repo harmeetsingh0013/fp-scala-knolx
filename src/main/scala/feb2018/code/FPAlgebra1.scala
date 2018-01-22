25dbcb12312467cd17b9f33be26feb5077100038
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-01-22 21:53:47 +0530

    Remove fpalgebra 1 and 2 code

--- src/main/scala/feb2018/code/FPAlgebra1.scala
+++ src/main/scala/feb2018/code/FPAlgebra1.scala
@@ -1,22 +1,4 @@
 package feb2018.code
 
 object FPAlgebra1 extends App {
-
-  def f(i : Int): Int = {
-    println(s"f input value $i")
-    i * 2
-  }
-
-  def g(j: Int): Int = {
-    println(s"g input value $j")
-    j + 2
-  }
-
-  def h(k: Int): Int = {
-    println(s"h input value $k")
-    k + 1
-  }
-
-  val result = h(g(f(2)))
-  println(s"Result is $result")
 }
--- src/main/scala/feb2018/code/FPAlgebra2.scala
+++ src/main/scala/feb2018/code/FPAlgebra2.scala
@@ -1,24 +1,4 @@
 package feb2018.code
 
 object FPAlgebra2 extends App {
-  def f(i : Int): (Int, String) = {
-    (i * 2, s"f input value $i")
-  }
-
-  def g(j: Int): (Int, String)  = {
-    (j + 2, s"g input value $j")
-  }
-
-  def h(k: Int): (Int, String)  = {
-    (k + 1, s"h input value $k")
-  }
-
-  // val result = h(g(f(2)))
-
-  val fResult = f(2)
-  val gResult = g(fResult._1)
-  val hResult = h(gResult._1)
-
-  val logs = fResult._2 + " " + gResult._2 + " " + hResult._2
-  println(s"Result ${ hResult._1 }, logs: $logs")
 }
